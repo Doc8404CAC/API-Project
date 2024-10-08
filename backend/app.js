8684e755c72c8a8ba51150beed99d9729b84c24e
KevinFugate <kevin.l.fugate@gmail.com>
2024-10-07 19:15:22 -0700

    phase 2 complete

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,7 @@ const routes = require('./routes');
 
 const { environment } = require('./config');
 const isProduction = environment === 'production';
+const { ValidationError } = require('sequelize');
 
 const app = express();
 
@@ -19,6 +20,30 @@ app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(express.json());
 app.use(routes); // Connect all the routes
+
+
+// Catch unhandled requests and forward to error handler.
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = { message: "The requested resource couldn't be found." };
+    err.status = 404;
+    next(err);
+  });
+
+  // Process sequelize errors
+app.use((err, _req, _res, next) => {
+    // check if error is a Sequelize error:
+    if (err instanceof ValidationError) {
+      let errors = {};
+      for (let error of err.errors) {
+        errors[error.path] = error.message;
+      }
+      err.title = 'Validation error';
+      err.errors = errors;
+    }
+    next(err);
+  });
 
 // Security Middleware
 if (!isProduction) {
@@ -43,5 +68,19 @@ app.use(
         }
     })
 );
+
+// Error formatter
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+      title: err.title || 'Server Error',
+      message: err.message,
+      errors: err.errors,
+      stack: isProduction ? null : err.stack
+    });
+  });
+
+
 
 module.exports = app;
